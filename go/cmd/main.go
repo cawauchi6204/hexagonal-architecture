a0f618d01b9413b0a1c0a1ff2155ffb5c8a3a9dd
河内達也 <cawauchi6204@gmail.com>
2024-08-25 18:00:56 +0900

    ✨ port番号を環境変数から取得するようにした

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -1,11 +1,22 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
 	e := echo.New()
+
+	// 環境変数からポート番号を取得
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // デフォルトのポート番号
+	}
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(200, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
