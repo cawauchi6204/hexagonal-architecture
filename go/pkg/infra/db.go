796db3ba3941bcea8f1364392bf0887489ef1e86
cawauchi6204 <cawauchi6204@gmail.com>
2024-10-04 11:21:40 +0900

    ✨ debugログを出せるようにした

--- go/pkg/infra/db.go
+++ go/pkg/infra/db.go
@@ -9,6 +9,7 @@ import (
 	"github.com/rs/zerolog"
 	sqldblogger "github.com/simukti/sqldb-logger"
 	"github.com/simukti/sqldb-logger/logadapter/zerologadapter"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 func ReadDSN() string {
@@ -20,19 +21,10 @@ func ReadDSN() string {
 	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, password, dbname, port, dbname)
 }
 
-// func ReadDSN() string {
-// 	host := os.Getenv("DB_HOST")
-// 	port := os.Getenv("DB_PORT")
-// 	user := os.Getenv("DB_USER")
-// 	password := os.Getenv("DB_PASSWORD")
-// 	dbname := os.Getenv("DB_NAME")
-
-// 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-// }
-
 // InitDB db接続を初期化
 func InitDB() *sql.DB {
 	loggerAdapter := zerologadapter.New(zerolog.New(os.Stdout))
+	boil.DebugMode = true
 
 	db := sqldblogger.OpenDriver(
 		ReadDSN(),
